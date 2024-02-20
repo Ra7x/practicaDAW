685339171466655fc49525d75c853d1d29a9ed22
Ra7x <yusei.d.runner@gmail.com>
2024-02-20 10:34:53 +0100

    Primer comit :D

---
+++ src/practicadawrms/PracticaDawRMS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicadawrms;
+
+/**
+ *
+ * @author Ra7x
+ */
+public class PracticaDawRMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        miniCalculadoreEjemplo calculadora = new miniCalculadoreEjemplo();
+        
+        double resultadoRaizCuadrada = calculadora.raizCuadrada(25);
+        System.out.println(resultadoRaizCuadrada);
+        
+        double valorAbsoluto = calculadora.valorAbsoluto(3.14);
+        System.out.println(valorAbsoluto);
+    }
+    
+}
---
+++ src/practicadawrms/miniCalculadoreEjemplo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicadawrms;
+
+/**
+ *
+ * @author Ra7x
+ */
+public class miniCalculadoreEjemplo {
+
+    // Método para calcular el valor absoluto de un número double
+    public double valorAbsoluto(double numero) {
+        return Math.abs(numero);
+    }
+
+    // Método para calcular la raiz cuadada de un número double
+    public double raizCuadrada(double numero) {
+        return Math.sqrt(numero);
+    }
+}
